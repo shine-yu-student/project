760054b4bc2852ecef449f828534092bbd834b8e
shine-yu-student <13512721598@139.com>
2022-06-05 23:21:06 +0800

    update-2022-6-5

---
+++ lib/bfs_tmp.hpp
@@ -0,0 +1,13 @@
+#define _USE_BFS_TMP
+#ifdef _USE_BFS_TMP
+#include<iostream>
+#include<utility>
+#include<queue>
+class bfs_template{
+	struct datapack{
+		size_t searchRounds;
+		std::vector<size_t> searchRoad;
+		
+	};
+};
+#endif
